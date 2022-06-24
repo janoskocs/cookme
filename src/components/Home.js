e19f9276973008dc07fcf9a4df1ec4ed43d2bbb8
Janos Kocs <janos.kocs@outlook.com>
2022-06-24 22:08:23 +0100

    Fetch blog data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
-
+import { useState, useEffect } from "react"
+import BlogList from "./BlogList"
 const Home = () => {
+
+    const [blogs, setBlogs] = useState(null);
+
+    useEffect(() => {// can't use async here
+        fetch('http://localhost:8000/blogs').then(response => {
+            return response.json() //THis parses the response to javascript object. Because this is asynchronous, it will return another promise.
+        }).then(data => { //Need another .then to get the actual data from the response above
+            //This data contains the actual data
+            console.log(data)
+            setBlogs(data)//Update blog state with data received
+        })
+    }, []) //Pass empty array as 2nd argument to run useEffect once when components render
+
+
     return (
-        <div>Home</div>
+        <div className="content">
+            {/*Check if blogs is true and not null, then render blogs, otherwise if it's false it will not output bloglist component */blogs && <BlogList blogs={blogs} title="All blogs" />}
+        </div>
     )
 }
 
