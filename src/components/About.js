602505a277f2ca60fcdee028174b1537b4b45b63
Janos Kocs <janos.kocs@outlook.com>
2022-06-26 22:38:26 +0100

    About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const About = () => {
     return (
-        <div>About</div>
+        <div>
+            <h2 className="section-title">Thank you for your visit!</h2>
+            <p className="section-description">This is a personal portfolio project. If you like this website then let's connect! Please visit my portfolio website at <a href="https://janoskocs.com">janoskocs.com</a></p>
+        </div>
     )
 }
 
